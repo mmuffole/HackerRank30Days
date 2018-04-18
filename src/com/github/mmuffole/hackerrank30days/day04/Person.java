e5b59c1faf3fc2230f804fba8eda3cc668c6d889
Maria <mmuffole@gmail.com>
2018-04-17 19:29:46 -0500

    GOT THE CODE TO COMPILE!! Added some variables into the main method. Still not doing what I intend, but at least it compiles now!!

--- src/com/github/mmuffole/hackerrank30days/day04/Person.java
+++ src/com/github/mmuffole/hackerrank30days/day04/Person.java
@@ -5,41 +5,40 @@ import java.util.Scanner;
 
 public class Person {
 
-        private int age;
+    private int age;
 
-        public Person(int initialAge) {
-            String ansPerson = "";
-            if(initialAge < 0){
-               ansPerson = "Age is not valid, setting age to 0.";
-            }
-            age = initialAge;
-
-            System.out.println(ansPerson);
+    public Person(int initialAge) {
+        String ansPerson = "";
+        if (initialAge < 0) {
+            ansPerson = "Age is not valid, setting age to 0.";
         }
+        age = initialAge;
 
-        public void amIOld(int initialAge) {
-            // Write code determining if this person's age is old and print the correct statement:
-            String ansAmIOld = "";
-            if(initialAge < 13) {
-                ansAmIOld = "You are young.";
-            }
-            else if(initialAge >= 13 && initialAge < 18) {
-                ansAmIOld = "You are a teenager.";
-            }
-            else if(initialAge > 18) {
-                ansAmIOld = "You are old.";
-            }
-                System.out.println(ansAmIOld);
-        }
+        System.out.println(ansPerson);
+    }
 
-        public void yearPasses() {
-            // Increment this person's age.
-            String ansYearPasses = "";
-            for (int yearPasses = 0; yearPasses < 100; yearPasses++); {
-                System.out.println(ansYearPasses);
-            }
+    public void amIOld(int initialAge) {
+        // Write code determining if this person's age is old and print the correct statement:
+        String ansAmIOld = "";
+        if (initialAge < 13) {
+            ansAmIOld = "You are young.";
+        } else if (initialAge >= 13 && initialAge < 18) {
+            ansAmIOld = "You are a teenager.";
+        } else if (initialAge > 18) {
+            ansAmIOld = "You are old.";
         }
-}
+        System.out.println(ansAmIOld);
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+        String ansYearPasses = "";
+        for (int yearPasses = 0; yearPasses < 100; yearPasses++) ;
+        {
+            System.out.println(ansYearPasses);
+        }
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -47,13 +46,13 @@ public class Person {
         for (int i = 0; i < T; i++) {
             int age = sc.nextInt();
             Person p = new Person(age);
-            p.amIOld(initialAge);
+            p.amIOld(i);
             for (int j = 0; j < 3; j++) {
                 p.yearPasses();
             }
-            p.amIOld();
+            p.amIOld(i);
             System.out.println();
         }
         sc.close();
     }
-
+}
